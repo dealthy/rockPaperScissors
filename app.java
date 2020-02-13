03cfc46be09a0137af1b41f0ae198d379642b25e
dealthy <55175568+dealthy@users.noreply.github.com>
2020-02-13 10:55:03 +0800

    finalDebugAndChange

--- app.java
+++ app.java
@@ -5,11 +5,25 @@
  * The main method output teh result and take in the input
  */
 import java.util.Scanner;
-import java.lang.Random;
+import java.util.Random;
 public class app{
     public static String winner(int human){
         // cases in total
-        int computer = Random(2);
+        Random rand = new Random();
+        String computerChoice = "";
+        int computer = rand.nextInt(2);
+        switch(computer){
+            case 0: {
+                computerChoice = "rock";
+            }
+            case 1: {
+                computerChoice = "paper";
+            }
+            case 2: {
+                computerChoice = "scissors";
+            }
+        }
+        System.out.println("Computer's choice is: " + computerChoice);
         if(human == computer){
             return "draw";
             //all draw cases (3)
@@ -27,12 +41,11 @@ public class app{
     }
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
+        System.out.println("welcome to rock paper scissors");
         while (true){
-            System.out.println("welcome to rock paper scissors");
-            System.out.println("rock = 0; paper = 1; scissors = 2");
+            System.out.print("rock = 0; paper = 1; scissors = 2\n" + "Your choice is: ");
             int choice = scan.nextInt();
-            System.out.println("The Winner is:" + winner(choice));
+            System.out.println("The Winner is: " + winner(choice) + "\n");
         }
     }
 }
