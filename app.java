5137b854719a5cd14fb4e45a29a937b78259e3d2
Harry Yu <55175568+dealthy@users.noreply.github.com>
2020-08-26 11:00:19 +0800

    add bo3

--- app.java
+++ app.java
@@ -42,10 +42,39 @@ public class app{
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         System.out.println("welcome to rock paper scissors");
+        int scoreHuman = 0;
+        int scoreComputer = 0;
+        String winner = "";
+        //bo1
+        while (true){
+            System.out.print("rock = 0; paper = 1; scissors = 2; go to bo3 = 3\n" + "Your choice is: ");
+            int choice = scan.nextInt();
+            if(choice == 3){
+                break;
+            }
+            System.out.println("The Winner is: " + winner(choice));
+        }
+        //bo3
         while (true){
             System.out.print("rock = 0; paper = 1; scissors = 2\n" + "Your choice is: ");
             int choice = scan.nextInt();
-            System.out.println("The Winner is: " + winner(choice) + "\n");
+            winner = winner(choice);
+            if(winner == "computer"){
+                System.out.println("The Winner of the round is: " + winner);
+                scoreComputer += 1;
+            } else if(winner == "human"){
+                System.out.println("The Winner of the round is: " + winner);
+                scoreHuman += 1;
+            } else{
+                System.out.println("The round is draw");
+            }
+            if(scoreComputer == 2){
+                System.out.println("The Winner is: computer");
+                break;
+            } else if(scoreHuman == 2){
+                System.out.println("The Winner is: human");
+                break;
+            }
         }
     }
 }
